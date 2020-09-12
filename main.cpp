6cd815b5d9cc4885872a3eea48c61d26d6a52241
Артём <71020677+NIVEX1362@users.noreply.github.com>
2020-09-12 17:12:24 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,39 +5,6 @@
 int main()
     {
     txCreateWindow (1532 , 840);
-    txSetFillColour(TX_GRAY);
-    txSetColour(TX_BLACK);
-    txRectangle(0,0,1532,150);
-    txRectangle(1300,150,1532,840);
-    txSetFillColour(TX_WHITE);
-    txRectangle(0,150,1300,840);
-
-    txSetFillColour(TX_CYAN);
-    txRectangle( 10,10,200,140);
-    txSetFillColour(TX_BLUE);
-    txRectangle(210,10,400,140);
-    txSetFillColour(TX_GREEN);
-    txRectangle(410,10,600,140);
-    txSetFillColour(TX_RED);
-    txRectangle(610,10,800,140);
-    txSetFillColour(TX_MAGENTA);
-    txRectangle(810,10,1000,140);
-    txSetFillColour(TX_ORANGE);
-    txRectangle(1010,10,1200,140);
-
-    txSetColour(TX_ORANGE);
-    txSelectFont("ARIAL", 40);
-    txTextOut(70, 60,"Дома");
-    txSetColour(TX_MAGENTA);
-    txTextOut(250, 60,"Парки");
-    txSetColour(TX_RED);
-    txTextOut(450, 60,"Здании");
-    txSetColour(TX_GREEN);
-    txTextOut(650, 60,"Природа");
-    txSetColour(TX_BLUE);
-    txTextOut(850, 60,"Дороги");
-    txSetColour(TX_CYAN);
-    txTextOut(1020, 60,"Многоэтажки");
 
     HDC  object0 = txLoadImage ("dom1.bmp");
     HDC  object1 = txLoadImage ("dom2.bmp");
@@ -50,29 +17,82 @@ int main()
     HDC  object8 = txLoadImage ("park2.bmp");
     HDC  object9 = txLoadImage ("park3.bmp");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     txTextCursor (false);
+
+
+    while(true)
+    {
+
+        txSetFillColour(TX_GRAY);
+        txSetColour(TX_BLACK);
+        txRectangle(0,0,1532,150);
+        txRectangle(1300,150,1532,840);
+        txSetFillColour(TX_WHITE);
+        txRectangle(0,150,1300,840);
+
+        txSetFillColour(TX_CYAN);
+        txRectangle( 10,10,200,140);
+        txSetFillColour(TX_BLUE);
+        txRectangle(210,10,400,140);
+        txSetFillColour(TX_GREEN);
+        txRectangle(410,10,600,140);
+        txSetFillColour(TX_RED);
+        txRectangle(610,10,800,140);
+        txSetFillColour(TX_MAGENTA);
+        txRectangle(810,10,1000,140);
+        txSetFillColour(TX_ORANGE);
+        txRectangle(1010,10,1200,140);
+
+        txSetColour(TX_ORANGE);
+        txSelectFont("ARIAL", 40);
+        txTextOut(70, 60,"Дома");
+        txSetColour(TX_MAGENTA);
+        txTextOut(250, 60,"Парки");
+        txSetColour(TX_RED);
+        txTextOut(450, 60,"Здании");
+        txSetColour(TX_GREEN);
+        txTextOut(650, 60,"Природа");
+        txSetColour(TX_BLUE);
+        txTextOut(850, 60,"Дороги");
+        txSetColour(TX_CYAN);
+        txTextOut(1020, 60,"Многоэтажки");
+
+
+  while (txMouseButtons() != 3)
+              {
+              if (txMouseButtons() & 1)
+              (
+               (txMouseButtons()
+
+               )
+
+              if (txMouseButtons() & 2)
+              txSleep (0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        txSleep(20);
+    }
+
     return 0;
     }
 
