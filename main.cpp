253a4fc0516e53f5365e837e8113aa1a0c453c3d
Mikhail Abramov <beavisabra@yandex.ru>
2020-10-19 00:28:10 +0400

    Объединение-2

--- main.cpp
+++ main.cpp
@@ -18,42 +18,42 @@ int main()
 
     const int N_PICS = 24;
     Picture pic[N_PICS];
-    pic[0] = {1300, 200, 615, 369, txLoadImage ("Г„Г®Г¬Г /dom1.bmp"), "Home"};
-    pic[1] = {1300, 300, 564, 300, txLoadImage ("Г„Г®Г¬Г /dom2.bmp"), "Home"};
-    pic[2] = {1300, 400, 571, 393, txLoadImage ("Г„Г®Г¬Г /dom3.bmp"), "Home"};
-    pic[3] = {1300, 500, 350, 324, txLoadImage ("Г„Г®Г¬Г /dom4.bmp"), "Home"};
-    pic[4] = {1300, 600, 407, 264, txLoadImage ("Г„Г®Г¬Г /dom5.bmp"), "Home"};
-    pic[5] = {1300, 700, 495, 293, txLoadImage ("Г„Г®Г¬Г /dom6.bmp"), "Home"};
-    pic[6] = {1300, 550, 520, 342, txLoadImage ("Г„Г®Г¬Г /mnogoetajka1.bmp"), "Mnogoetajka1"};
-    pic[7] = {1300, 250, 900, 195, txLoadImage ("ГЏГ Г°ГЄГЁ/park1.bmp"), "Park"};
-    pic[8] = {1300, 400, 512, 355, txLoadImage ("ГЏГ Г°ГЄГЁ/park2.bmp"), "Park"};
-    pic[9] = {1300, 550, 650, 400, txLoadImage ("ГЏГ Г°ГЄГЁ/park3.bmp"), "Park"};
-    pic[10] = {1300, 250, 512, 429, txLoadImage ("Г„ГҐГ°ГҐГўГјГї/tree1.bmp"), "Tree"};
-    pic[11] = {1300, 400, 414, 484, txLoadImage ("Г„ГҐГ°ГҐГўГјГї/tree2.bmp"), "Tree"};
-    pic[12] = {1300, 550, 214, 236, txLoadImage ("Г„ГҐГ°ГҐГўГјГї/tree3.bmp"), "Tree"};
-    pic[13] = {1300, 250, 408, 581, txLoadImage ("Г„ГҐГ°ГҐГўГјГї/tree4.bmp"), "Tree"};
-    pic[14] = {1300, 400, 423, 512, txLoadImage ("Г„ГҐГ°ГҐГўГјГї/tree5.bmp"), "Tree"};
-    pic[15] = {1300, 550, 550, 412, txLoadImage ("Г„ГҐГ°ГҐГўГјГї/tree6.bmp"), "Tree"};
+    pic[0] = {1300, 200, 615, 369, txLoadImage ("Дома/dom1.bmp"), "Home"};
+    pic[1] = {1300, 300, 564, 300, txLoadImage ("Дома/dom2.bmp"), "Home"};
+    pic[2] = {1300, 400, 571, 393, txLoadImage ("Дома/dom3.bmp"), "Home"};
+    pic[3] = {1300, 500, 350, 324, txLoadImage ("Дома/dom4.bmp"), "Home"};
+    pic[4] = {1300, 600, 407, 264, txLoadImage ("Дома/dom5.bmp"), "Home"};
+    pic[5] = {1300, 700, 495, 293, txLoadImage ("Дома/dom6.bmp"), "Home"};
+    pic[6] = {1300, 550, 520, 342, txLoadImage ("Дома/mnogoetajka1.bmp"), "Mnogoetajka1"};
+    pic[7] = {1300, 250, 900, 195, txLoadImage ("Парки/park1.bmp"), "Park"};
+    pic[8] = {1300, 400, 512, 355, txLoadImage ("Парки/park2.bmp"), "Park"};
+    pic[9] = {1300, 550, 650, 400, txLoadImage ("Парки/park3.bmp"), "Park"};
+    pic[10] = {1300, 250, 512, 429, txLoadImage ("Деревья/tree1.bmp"), "Tree"};
+    pic[11] = {1300, 400, 414, 484, txLoadImage ("Деревья/tree2.bmp"), "Tree"};
+    pic[12] = {1300, 550, 214, 236, txLoadImage ("Деревья/tree3.bmp"), "Tree"};
+    pic[13] = {1300, 250, 408, 581, txLoadImage ("Деревья/tree4.bmp"), "Tree"};
+    pic[14] = {1300, 400, 423, 512, txLoadImage ("Деревья/tree5.bmp"), "Tree"};
+    pic[15] = {1300, 550, 550, 412, txLoadImage ("Деревья/tree6.bmp"), "Tree"};
     pic[16] = {1300, 200, 300, 196, txLoadImage ("Zdania/Zdania1.bmp"), "Zdania"};
     pic[17] = {1300, 300, 256, 256, txLoadImage ("Zdania/Zdania2.bmp"), "Zdania"};
     pic[18] = {1300, 400, 360, 360, txLoadImage ("Zdania/Zdania3.bmp"), "Zdania"};
     pic[19] = {1300, 500, 260, 260, txLoadImage ("Zdania/Zdania4.bmp"), "Zdania"};
     pic[20] = {1300, 600, 360, 360, txLoadImage ("Zdania/Zdania5.bmp"), "Zdania"};
-    pic[21] = {1325, 250, 150, 77, txLoadImage ("Г„Г®Г°Г®ГЈГЁ/doroga1.bmp"), "Roads"};
-    pic[22] = {1325, 400, 300, 89, txLoadImage ("Г„Г®Г°Г®ГЈГЁ/doroga2.bmp"), "Roads"};
-    pic[23] = {1325, 600, 300, 300, txLoadImage ("Г„Г®Г°Г®ГЈГЁ/doroga3.bmp"), "Roads"};
+    pic[21] = {1325, 250, 150, 77, txLoadImage ("Дороги/doroga1.bmp"), "Roads"};
+    pic[22] = {1325, 400, 300, 89, txLoadImage ("Дороги/doroga2.bmp"), "Roads"};
+    pic[23] = {1325, 600, 300, 300, txLoadImage ("Дороги/doroga3.bmp"), "Roads"};
 
     int n_pics = 0;
     Picture center[2000];
 
     const int N_Button = 6;
     Button buttons[N_Button];
-    buttons[0] = {10, 10, "Г„Г®Г¬Г ",TX_CYAN,TX_YELLOW, "Home"};
-    buttons[1] = {210, 10, "ГЏГ Г°ГЄГЁ",TX_GREEN,TX_ORANGE, "Park"};
-    buttons[2] = {410, 10, "Г‡Г¤Г Г­ГЁГЁ",TX_BLUE,TX_MAGENTA, "Zdania"};
-    buttons[3] = {610, 10, "ГЏГ°ГЁГ°Г®Г¤Г ",TX_MAGENTA,TX_BLUE, "Tree"};
-    buttons[4] = {810, 10, "Г„Г®Г°Г®ГЈГЁ",TX_ORANGE,TX_GREEN, "Roads"};
-    buttons[5] = {1010, 10, "ГЊГ­Г®ГЈГ®ГЅГІГ Г¦ГЄГЁ",TX_YELLOW,TX_CYAN};
+    buttons[0] = {10, 10, "Дома",TX_CYAN,TX_YELLOW, "Home"};
+    buttons[1] = {210, 10, "Парки",TX_GREEN,TX_ORANGE, "Park"};
+    buttons[2] = {410, 10, "Здании",TX_BLUE,TX_MAGENTA, "Zdania"};
+    buttons[3] = {610, 10, "Природа",TX_MAGENTA,TX_BLUE, "Tree"};
+    buttons[4] = {810, 10, "Дороги",TX_ORANGE,TX_GREEN, "Roads"};
+    buttons[5] = {1010, 10, "Многоэтажки",TX_YELLOW,TX_CYAN};
 
 
 
@@ -100,10 +100,10 @@ int main()
 
         drawRightPictures(N_PICS, pic, category);
 
-        //ГђГЁГ±Г®ГўГ Г­ГЁГҐ Г¶ГҐГ­ГІГ°Г Г«ГјГ­Г»Гµ ГЄГ Г°ГІГЁГ­Г®ГЄ
+        //Рисование центральных картинок
         drawCentralPictures(n_pics, center);
 
-        //ГЇГ®ГїГўГ«ГҐГ­ГЁГҐ Г ГЄГІГЁГўГ­Г®Г© ГЄГ Г°ГІГЁГ­ГЄГЁ
+        //появление активной картинки
         for (int i = 0; i < N_PICS; i++)
         {
             if (txMouseButtons() == 1 &&
@@ -116,7 +116,7 @@ int main()
             }
         }
 
-        //Г“Г¤Г Г«ГҐГ­ГЁГҐ
+        //Удаление
         if(GetAsyncKeyState(VK_DELETE))
         {
             n_pics = n_pics - 1;
