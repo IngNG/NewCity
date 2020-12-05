cd90ad867d478e7be53c16b0c5a248fad94bc63c
Vladislav <51991300+PechenkA73@users.noreply.github.com>
2020-12-05 17:00:37 +0300

    сделал дороги

--- main3.cpp
+++ main3.cpp
@@ -8,18 +8,31 @@ int main()
 {
     txCreateWindow (1532 , 840);
 
-    HDC pic1 = txLoadImage("Дороги/dorogaLeftRight.bmp");
-    HDC pic2 = txLoadImage("Дороги/doroga2.bmp");
-    HDC pic3 = txLoadImage("Дороги/dorogaUpDown.bmp");
+    HDC picLeftRight = txLoadImage("Дороги/dorogaLeftRight.bmp");
+    HDC picLeftUp = txLoadImage("Дороги/dorogaLeftUp.bmp");
+    HDC picLeftDown = txLoadImage("Дороги/dorogaLeftDown.bmp");
+    HDC picRightDown = txLoadImage("Дороги/dorogaRightDown.bmp");
+    HDC picRightUp = txLoadImage("Дороги/dorogaRightUp.bmp");
+    HDC picUpDown = txLoadImage("Дороги/dorogaUpDown.bmp");
+    HDC pic4 = txLoadImage("Дороги/doroga4.bmp");
+    HDC pic3Up = txLoadImage("Дороги/doroga3Up.bmp");
+    HDC pic3Down = txLoadImage("Дороги/doroga3Down.bmp");
+    HDC pic3Right = txLoadImage("Дороги/doroga3Right.bmp");
+    HDC pic3Left = txLoadImage("Дороги/doroga3Left.bmp");
+    HDC picTupikUp = txLoadImage("Дороги/dorogaTupikUp.bmp");
+    HDC picTupikDown = txLoadImage("Дороги/dorogaTupikDown.bmp");
+    HDC picTupikRight = txLoadImage("Дороги/dorogaTupikRight.bmp");
+    HDC picTupikLeft = txLoadImage("Дороги/dorogaTupikLeft.bmp");
 
-    bool dorogi[10][10];
+    bool dorogi[30][30];
 
-    for(int i = 0; i < 10; i++)
-    for(int j = 0; j < 10; j++)
+    for(int i = 0; i < 30; i++)
+    for(int j = 0; j < 30; j++)
     dorogi[i][j] = false;
 
-    while(true)
+    while(!GetAsyncKeyState(VK_ESCAPE))
     {
+        txBegin();
         txSetFillColor(TX_WHITE);
         txClear();
 
@@ -29,18 +42,49 @@ int main()
         if (txMouseButtons() == 1)
             dorogi [x][y] = true;
 
-        for(int i = 0; i < 10; i++)
-        for(int j = 0; j < 10; j++)
+        for(int i = 0; i < 30; i++)
+        for(int j = 0; j < 30; j++)
         if(dorogi[i][j])
         {
-            if(dorogi[i-1][j] && dorogi[i][j-1])
-                txBitBlt(txDC(), 100 * i,100 * j, 100, 100, pic2);
+            if(dorogi[i+1][j] && dorogi[i-1][j] && dorogi[i][j-1] && dorogi[i][j+1])
+                txBitBlt(txDC(), 100 * i,100 * j, 100, 100, pic4);
+
+            else if(dorogi[i+1][j] && dorogi[i-1][j] && dorogi[i][j-1])
+                txBitBlt(txDC(), 100 * i,100 * j, 100, 100, pic3Up);
+            else if(dorogi[i+1][j] && dorogi[i-1][j] && dorogi[i][j+1])
+                txBitBlt(txDC(), 100 * i,100 * j, 100, 100, pic3Down);
+            else if(dorogi[i+1][j] && dorogi[i][j-1] && dorogi[i][j+1])
+                txBitBlt(txDC(), 100 * i,100 * j, 100, 100, pic3Right);
+            else if(dorogi[i-1][j] && dorogi[i][j-1] && dorogi[i][j+1])
+                txBitBlt(txDC(), 100 * i,100 * j, 100, 100, pic3Left);
+
+            else if(dorogi[i-1][j] && dorogi[i][j-1])
+                txBitBlt(txDC(), 100 * i,100 * j, 100, 100, picLeftUp);
+            else if(dorogi[i-1][j] && dorogi[i][j+1])
+                txBitBlt(txDC(), 100 * i,100 * j, 100, 100, picLeftDown);
+            else if(dorogi[i+1][j] && dorogi[i][j+1])
+                txBitBlt(txDC(), 100 * i,100 * j, 100, 100, picRightDown);
+            else if(dorogi[i+1][j] && dorogi[i][j-1])
+                txBitBlt(txDC(), 100 * i,100 * j, 100, 100, picRightUp);
+
             else if (dorogi[i][j+1] && dorogi[i][j-1])
-                txBitBlt(txDC(), 100 * i,100 * j, 100, 100, pic3);
-            else
-                txBitBlt(txDC(), 100 * i,100 * j, 100, 100, pic1);
+                txBitBlt(txDC(), 100 * i,100 * j, 100, 100, picUpDown);
+            else if (dorogi[i+1][j] && dorogi[i-1][j])
+                txBitBlt(txDC(), 100 * i,100 * j, 100, 100, picLeftRight);
+
+            else if (dorogi[i-1][j])
+                txBitBlt(txDC(), 100 * i,100 * j, 100, 100, picTupikRight);
+            else if (dorogi[i+1][j])
+                txBitBlt(txDC(), 100 * i,100 * j, 100, 100, picTupikLeft);
+            else if (dorogi[i][j+1])
+                txBitBlt(txDC(), 100 * i,100 * j, 100, 100, picTupikUp);
+            else if (dorogi[i][j-1])
+                txBitBlt(txDC(), 100 * i,100 * j, 100, 100, picTupikDown);
+
+
         }
         txSleep(10);
+        txEnd();
     }
 
 }
